d5dc2fd157aeed05905f23f482bd28bd661fcc0a
Neohack00 <neo@bylock.se>
2019-11-18 14:09:36 +0100

    Added connections to AboutUs and Static some folders. CSS is working! Up and running at https://gentle-inlet-60219.herokuapp.com/

--- Server.js
+++ Server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/Sidor/html/index.html');
-  
-
-
+    res.sendfile(__dirname + '/Sidor/html/index.html');
 })
+app.get(__dirname + '/html/AboutUs.html', function(req, res){
+    res.sendfile(__dirname + "/Sidor/html/info.html");
+})
+app.use(express.static('Sidor'))
+app.use(express.static('Sidor/html'))
 
-
-var server = app.listen(process.env.PORT, function(){
+var server = app.listen(1337, function(){
     console.log('Server is online on port ' + server.address().port);
 })
